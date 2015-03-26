fdfdf745bd8d6c4e5d90ab91b2107889a2d88197
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-03-26 17:48:50 -0500

    EF project, exclude app.config

---
+++ Data.Sql/PiiaDb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Sql
+{
+    public class PiiaDb : DbContext
+    {
+        static PiiaDb()
+        {
+            Database.SetInitializer<PiiaDb>(null);
+        }
+
+        public PiiaDb()
+            : base("Name=piia")
+        {
+        }
+
+        public int? GetCommandTimeout()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.CommandTimeout;
+        }
+
+        public void SetCommandTimeout(int? timeoutInSeconds)
+        {
+            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = timeoutInSeconds;
+        }
+
+        public void ResetCommandTimeout()
+        {
+            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = null;
+        }
+
+        public DbContextTransaction BeginTransaction()
+        {
+            return Database.BeginTransaction();
+        }
+
+        public DbRawSqlQuery<TElement> SqlQuery<TElement>(string sql, params object[] parameters)
+        {
+            return Database.SqlQuery<TElement>(sql, parameters);
+        }
+
+        public Task<T> SingleOrDefaultAsync<T>(string sql, params object[] parameters)
+        {
+            return Database.SqlQuery<T>(sql, parameters).SingleOrDefaultAsync();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- Data.Sql/Properties/AssemblyInfo.cs
+++ Data.Sql/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Scrambles")]
+[assembly: AssemblyTitle("Data.Sql")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Scrambles")]
+[assembly: AssemblyProduct("Data.Sql")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("45638a98-f5f7-4cfb-895c-6a6e76c3de90")]
+[assembly: Guid("c4aef880-b0f8-4932-904c-d802f1925ab0")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
