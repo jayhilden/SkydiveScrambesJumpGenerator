ca90be2db11abea8a08b5b209491c068bcbb685a
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-04-05 19:04:11 -0500

    #3 round list page now displays images

--- Data.Sql/Models/Round.cs
+++ Data.Sql/Models/Round.cs
@@ -10,9 +10,16 @@ namespace Data.Sql.Models
     public class Round
     {
         public int RoundID { get; set; }
+
+        [Display(Name = "Round Number")]
         public int RoundNumber { get; set; }
 
         [Required]
         public string Formations { get; set; }
+
+        public IEnumerable<string> FormationsSplit
+        {
+            get { return Formations.Split(','); }
+        }
     }
 }
---
+++ Scrambles/Controllers/RoundsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Data.Sql;
+using Data.Sql.Models;
+
+namespace Scrambles.Controllers
+{
+    public class RoundsController : Controller
+    {
+        private PiiaDb db = new PiiaDb();
+
+        // GET: Rounds
+        public ActionResult Index()
+        {
+            return View(db.Rounds.ToList());
+        }
+
+        // GET: Rounds/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Round round = db.Rounds.Find(id);
+            if (round == null)
+            {
+                return HttpNotFound();
+            }
+            return View(round);
+        }
+
+        // GET: Rounds/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rounds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RoundID,RoundNumber,Formations")] Round round)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Rounds.Add(round);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(round);
+        }
+
+        // GET: Rounds/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Round round = db.Rounds.Find(id);
+            if (round == null)
+            {
+                return HttpNotFound();
+            }
+            return View(round);
+        }
+
+        // POST: Rounds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RoundID,RoundNumber,Formations")] Round round)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(round).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(round);
+        }
+
+        // GET: Rounds/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Round round = db.Rounds.Find(id);
+            if (round == null)
+            {
+                return HttpNotFound();
+            }
+            return View(round);
+        }
+
+        // POST: Rounds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Round round = db.Rounds.Find(id);
+            db.Rounds.Remove(round);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
