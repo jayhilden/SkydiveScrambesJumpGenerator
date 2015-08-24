9d2d54f6475603a0f23b399e76d6264f6dae8bc4
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-08-24 08:52:44 -0500

    #17 new column to store camera man

--- Data.Sql/Migrations/201508241350171_JumpScore_17.cs
+++ Data.Sql/Migrations/201508241350171_JumpScore_17.cs
@@ -8,10 +8,12 @@ namespace Data.Sql.Migrations
         public override void Up()
         {
             AddColumn("dbo.RoundJumperMap", "Score", c => c.Int());
+            AddColumn("dbo.RoundJumperMap", "Camera", c => c.String(maxLength: 100));
         }
         
         public override void Down()
         {
+            DropColumn("dbo.RoundJumperMap", "Camera");
             DropColumn("dbo.RoundJumperMap", "Score");
         }
     }
--- Data.Sql/Models/RoundJumperMap.cs
+++ Data.Sql/Models/RoundJumperMap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -26,6 +27,9 @@ namespace Data.Sql.Models
         public JumpGroupFlag JumpGroup { get; set; }
 
         public int? Score { get; set; }
+
+        [MaxLength(100)]
+        public string Camera { get; set; }
 
         public virtual Round Round { get; set; }
         
