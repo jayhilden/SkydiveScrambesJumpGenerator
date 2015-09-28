7b6fe653457a0ad33528471e577239369a0dbf32
Jay Hilden <Jay.Hilden@verifycorporation.com>
2015-09-28 17:27:47 -0500

    #18 summary results are looking very good

---
+++ Scrambles/Models/ResultsListRow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrambles.Models
+{
+    public class ResultsListRow
+    {
+        public string Name { get; set; }
+        public int TotalScore { get; set; }
+    }
+}
